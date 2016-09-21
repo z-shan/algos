34dcde6f4470d9f25b72fe80481065dbd0a0044e
Zeeshan <zeeshan@ku.edu>
2016-09-20 21:16:30 -0500

    first index of substring

--- first-occurance-of-substring.js
+++ first-occurance-of-substring.js
@@ -4,15 +4,28 @@
  * @return {number}
  * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
  */
-var strStr = function(haystack, needle) {
-    if(haystack == needle) {
-        return 0;
-    } else {
-        for(var i=0; i<haystack.length; i++) {
-            if(haystack.substr(i, needle.length) == needle) {
-                return i;
-            }
+var indexOfSubstr = function(haystack, needle) {
+    for(var i=0; i<haystack.length; i++) {
+        if(haystack.substr(i, needle.length) == needle) {
+            return i;
         }
-        return -1;
     }
+    return -1;
+};
+
+
+// without using substr method
+var indexOfSubstr2 = function(haystack, needle) {
+    for(var i=0, j=0; i<haystack.length && j<needle.length; i++) {
+        if(haystack.charAt(i) == needle.charAt(j)) {
+            if(j == needle.length - 1) {
+                return i - needle.length + 1;
+            }
+            j++;
+        } else {
+            i -= j;
+            j=0;
+        }
+    }
+    return needle.length === 0 ? 0 : -1;
 };
